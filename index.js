00c8c06b037444d91ee2adf1874c2c98228e4fca
Justin Lee <jlee201@lion.lmu.edu>
2018-12-04 15:59:21 -0800

    Users can see survey results

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -13,24 +14,25 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production assets 
+	 // Express will serve up production assets
 	// like our main.js file, or main.css file!
 	app.use(express.static('client/build'));
 
-	// Express will serve up the index.html file 
+	// Express will serve up the index.html file
 	// if it doesn't recognize the route
 	const path = require('path');
 	app.get('*', (req, res) => {
